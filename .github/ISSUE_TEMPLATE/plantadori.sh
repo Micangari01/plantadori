6c44ec0de55f08101ecd05809009d333e7a7ac26
Micangari01 <137118864+Micangari01@users.noreply.github.com>
2023-06-19 19:33:34 -0300

    Update plantadori.sh
    Este script atualizado adiciona a possibilidade do usuário inserir o nome de usuário e a senha do Git, bem como o endereço do servidor 1. A imagem do Alpine Linux, o arquivo SHA256 e o arquivo GPG são baixados diretamente dos links fornecidos no script. A verificação de segurança é feita comparando o valor SHA256 do arquivo baixado com o valor fornecido no arquivo de verificação SHA256.

--- .github/ISSUE_TEMPLATE/plantadori.sh
+++ .github/ISSUE_TEMPLATE/plantadori.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # Variáveis de configuração
-PENDRIVE_DEV=$(lsblk -o NAME,SIZE,TYPE,MOUNTPOINT | awk '$2 == "8G" && $3 == "disk" && $4 == "" {print $1}')
-ALPINE_ISO="alpine-linux-extended.iso"
 PROFILE_FILE="/mnt/profile.txt"
 
-# Função para obter o endereço GIT
-get_git_address() {
-    read -p "Informe o endereço GIT para download da imagem do MiçangáriOS: " GIT_ADDRESS
-    echo "$GIT_ADDRESS"
+# Função para obter as credenciais do Git
+get_git_credentials() {
+    read -p "Informe o nome de usuário do Git: " GIT_USERNAME
+    read -s -p "Informe a senha de acesso do Git: " GIT_PASSWORD
+    echo
+}
+
+# Função para obter o endereço do servidor 1
+get_server1_address() {
+    read -p "Informe o endereço do servidor 1: " LINK_SERVIDOR1
+    echo "$LINK_SERVIDOR1"
 }
 
 # Verifica se o pendrive foi identificado corretamente
+PENDRIVE_DEV=$(lsblk -o NAME,SIZE,TYPE,MOUNTPOINT | awk '$2 == "8G" && $3 == "disk" && $4 == "" {print $1}')
 read -p "O pendrive foi identificado como ${PENDRIVE_DEV}. Deseja prosseguir? (s/n): " choice
 if [ "$choice" != "s" ]; then
     read -p "Informe o caminho completo para o dispositivo do pendrive: " PENDRIVE_DEV
@@ -43,30 +49,58 @@ if [ -e "$PROFILE_FILE" ]; then
     # Carrega as configurações do perfil existente e inicia o sistema
     # Coloque aqui o código para carregar as configurações do perfil
     echo "Arquivo de perfil encontrado. Carregando as configurações..."
-    GIT_ADDRESS=$(grep "GIT_ADDRESS" "$PROFILE_FILE" | cut -d'=' -f2)
+    GIT_USERNAME=$(grep "GIT_USERNAME" "$PROFILE_FILE" | cut -d'=' -f2)
+    GIT_PASSWORD=$(grep "GIT_PASSWORD" "$PROFILE_FILE" | cut -d'=' -f2)
+    LINK_SERVIDOR1=$(grep "LINK_SERVIDOR1" "$PROFILE_FILE" | cut -d'=' -f2)
 else
-    # Obtém o endereço GIT para download da imagem do MiçangáriOS
-    echo "Arquivo de perfil não encontrado. Será necessário obter o endereço GIT."
-    GIT_ADDRESS=$(get_git_address)
+    # Obtém as informações do Git e o endereço do servidor 1
+    echo "Arquivo de perfil não encontrado. Será necessário obter as informações."
+    get_git_credentials
+    get_server1_address
 fi
 
-# Configuração do arquivo de configuração MAINPAGE do GIT
+# Configuração do arquivo de configuração MAINPAGE do Git
 cat > /mnt/git_config.cfg <<EOL
 # Configurações do usuário do MiçangáriOS
-GIT_USERNAME="seu_nome_de_usuário"
-GIT_PASSWORD="sua_senha_de_acesso"
+GIT_USERNAME="$GIT_USERNAME"
+GIT_PASSWORD="$GIT_PASSWORD"
 
 # Links para servidores e serviços adequados
-LINK_SERVIDOR1="https://servidor1.com"
-LINK_SERVIDOR2="https://servidor2.com"
+LINK_SERVIDOR1="$LINK_SERVIDOR1"
+LINK_SERVIDOR2="http://localhost"
 EOL
 
-# Código para download da imagem do MiçangáriOS do endereço GIT
-echo "Baixando a imagem do MiçangáriOS do endereço GIT: $GIT_ADDRESS"
-# Coloque aqui o código para fazer o download da imagem do MiçangáriOS do endereço GIT
+# Código para download da imagem do Alpine Linux, arquivos SHA256 e GPG
+ALPINE_VERSION="3.14.2"
+ARCHITECTURE="x86_64"
+ALPINE_ISO="alpine-standard-${ALPINE_VERSION}-${ARCHITECTURE}.iso"
+ALPINE_SHA256="${ALPINE_ISO}.sha256"
+ALPINE_GPG="${ALPINE_ISO}.asc"
+
+ALPINE_URL="https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/${ARCHITECTURE}/${ALPINE_ISO}"
+ALPINE_SHA256_URL="https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/${ARCHITECTURE}/${ALPINE_SHA256}"
+ALPINE_GPG_URL="https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/${ARCHITECTURE}/${ALPINE_GPG}"
+
+echo "Baixando a imagem do Alpine Linux do endereço: $ALPINE_URL"
+wget -P /mnt $ALPINE_URL
+
+echo "Baixando o arquivo SHA256 do endereço: $ALPINE_SHA256_URL"
+wget -P /mnt $ALPINE_SHA256_URL
+
+echo "Baixando o arquivo GPG do endereço: $ALPINE_GPG_URL"
+wget -P /mnt $ALPINE_GPG_URL
+
+# Verificação da imagem do Alpine Linux
+echo "Verificando a integridade da imagem do Alpine Linux..."
+cd /mnt
+sha256sum -c $ALPINE_SHA256
 
 # Desmonta a partição raiz do pendrive
 umount /mnt
+
+# Executa o MiçangáriOS diretamente do Git e acessa a partição de armazenamento persistente
+# Coloque aqui o código para executar o MiçangáriOS do Git e acessar a partição de armazenamento persistente
+
 
 # Executa o MiçangáriOS diretamente do GIT e acessa a partição de armazenamento persistente
 # Coloque aqui o código para executar o MiçangáriOS do GIT e acessar a partição de armazenamento persistente
